694feb0ccefcfb614284fd86e728172d35a02c3a
mcosta85 <costa.mason@gmail.com>
2019-02-16 14:50:36 -0500

    Add initial record parsing ns

--- src/record_sort/parse.clj
+++ src/record_sort/parse.clj
@@ -1,1 +1,18 @@
-(ns record-sort.parse)
+(ns record-sort.parse
+  (:require [clojure.string :as str]))
+
+(def ^:private delimiter-regex
+  "Match on the following pattern;
+
+  \\s? - Zero or one whitespace characters
+  [|,]? - Zero or one pipe or comma characters
+  \\s+ Exactly one whitespace character"
+
+  #"\s?[|,]?\s+")
+
+(defn parse-input
+  "Splits the input records on a regex which should match any delimiter used.
+  Returns a seq of record fields."
+  [input]
+  (str/split input delimiter-regex))
+
---
+++ test/record_sort/parse_test.clj
@@ -0,0 +1,15 @@
+(ns record-sort.parse-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is testing]]
+            [record-sort.parse :as parse]))
+
+(deftest parse-input-test
+  (testing "parsing of variously delimited records"
+    (let [comma-delimited "Last, First, Gender, FavColor, DOB"
+          pipe-delimited (str/replace comma-delimited #"," " |")
+          space-delimited (str/replace comma-delimited #"," "")
+          expected ["Last" "First" "Gender" "FavColor" "DOB"]]
+      (is (= expected (parse/parse-input comma-delimited)))
+      (is (= expected (parse/parse-input pipe-delimited)))
+      (is (= expected (parse/parse-input space-delimited))))))
+
