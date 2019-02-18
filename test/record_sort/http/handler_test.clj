a822e38ced6f2699699145df647575605dba44da
mcosta85 <costa.mason@gmail.com>
2019-02-18 15:53:49 -0500

    Add untracked files

---
+++ test/record_sort/http/handler_test.clj
@@ -0,0 +1,29 @@
+(ns record-sort.http.handler-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [deftest is testing]]
+            [record-sort.http.handler :as handler]))
+
+(def ^:private data-store
+  (atom ["Costa Mason Male Green 12/14/1985"
+         "Hopper Grace Female Black 12/9/1906"]))
+
+(deftest sort-by-gender-test
+  (testing "Response format"
+    (let [resp (handler/sort-by-gender data-store)]
+      (is (= 200 (:status resp)))
+      (is (get-in resp [:body :records]))
+      (is (= "gender" (get-in resp [:body :sorted-by]))))))
+
+(deftest sort-by-dob-test
+  (testing "Response format"
+    (let [resp (handler/sort-by-dob data-store)]
+      (is (= 200 (:status resp)))
+      (is (get-in resp [:body :records]))
+      (is (= "date of birth" (get-in resp [:body :sorted-by]))))))
+
+(deftest sort-by-name-test
+  (testing "Response format"
+    (let [resp (handler/sort-by-name data-store)]
+      (is (= 200 (:status resp)))
+      (is (get-in resp [:body :records]))
+      (is (= "last name" (get-in resp [:body :sorted-by]))))))
